c79a15f98cae419be4f5db7f4deb2d0299238e56
Roman Hrechukh <roman.grechukh@gmail.com>
2019-10-08 10:52:29 +0300

    3

--- E2ESeleniumJenkins/src/testcases/TC_001.java
+++ E2ESeleniumJenkins/src/testcases/TC_001.java
@@ -1,7 +1,7 @@
 package testcases;
 
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
 public class TC_001 {
@@ -9,8 +9,9 @@ public class TC_001 {
 	@Test
 	public void testcase1()
 	{
-		System.setProperty("webdriver.gecko.driver", "./driver/geckodriver.exe");
-		FirefoxDriver driver = new FirefoxDriver();
+		System.setProperty("webdriver.gecko.driver", "C:/Program Files/Gecko/geckodriver.exe");
+		DesiredCapabilities capabilities=DesiredCapabilities.firefox();
+		FirefoxDriver driver = new FirefoxDriver(capabilities);
 		driver.get("https://www.facebook.com");
 		driver.findElementById("email").sendKeys("Hello");
 		driver.findElementById("pass").sendKeys("Hello");
